9a0ea942e119ee1f05243ff684258c4dddf1a07b
Ashton Blignaut <41480774+MadBones3@users.noreply.github.com>
2021-12-07 17:43:24 +0200

    custom post types

---
+++ mu-plugins/my-post-types.php
@@ -0,0 +1,93 @@
+<?php
+
+// Custom post types
+function my_post_types() {
+
+// ========= EVENT =======================
+    register_post_type('event', array(
+        'supports' => array(
+            'title', 
+            'editor', 
+            'excerpt'
+        ),
+        'rewrite' => array(
+            'slug' => 'events'
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar-alt',
+        'show_in_rest' => true
+    ));
+
+// ======== PROGRAM =======================
+    register_post_type('program', array(
+        'supports' => array(
+            'title', 
+            'editor'
+        ),
+        'rewrite' => array(
+            'slug' => 'programs'
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards',
+        'show_in_rest' => true
+    ));
+
+// ======== PROFESSOR =======================
+    register_post_type('professor', array(
+        'supports' => array(
+            'title', 
+            'editor',
+            'thumbnail'
+        ),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professors',
+            'singular_name' => 'Professor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'show_in_rest' => true
+    ));
+
+// ======== CAMPUS =======================
+    register_post_type('campus', array(
+        'supports' => array(
+            'title', 
+            'editor',
+            'thumbnail'
+        ),
+        'rewrite' => array(
+            'slug' => 'campuses'
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Campuses',
+            'add_new' => 'Add New Campus',
+            'edit_item' => 'Edit Campus',
+            'all_items' => 'All Campuses',
+            'singular_name' => 'Campus'
+        ),
+        'menu_icon' => 'dashicons-location-alt',
+        'show_in_rest' => true
+    ));
+}
+add_action('init', 'my_post_types');
